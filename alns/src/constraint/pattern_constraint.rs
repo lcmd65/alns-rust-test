ddc2708e80e8756a2156d89a5c1c656af33b4343
Dat Le <dat@workforceoptimizer.com>
2024-10-07 13:28:48 +0700

    [feature] update

--- alns/src/constraint/pattern_constraint.rs
+++ alns/src/constraint/pattern_constraint.rs
@@ -6,10 +6,16 @@ pub struct PatternConstraint {
     pub (crate) id: String,
     pub (crate) description: String,
     pub (crate) constraint_type: String,
-    pub (crate) shift_patterns: Vec<String>,
+    pub (crate) shift_patterns: Vec<Vec<String>>,
     pub (crate) staff_groups: Vec<String>,
     pub (crate) priority: i8,
     pub (crate) exist: bool,
     pub (crate) is_hard: bool,
     pub (crate) penalty: i8,
+}
+
+impl PatternConstraint {
+    pub fn pattern_list(){
+
+    }
 }
